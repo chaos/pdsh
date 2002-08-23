20baecf0b83d7ef275b40f06969bed1796baabff
Mark A. Grondona <mgrondona@llnl.gov>
2002-08-23 16:44:55 +0000

    o have autogen.sh complain if autoconf, automake, or aclocal cannot be    found.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # $Id$
 # $Source$
 #
@@ -7,6 +8,38 @@
 # To specify extra flags to aclocal (include dirs for example),
 # set ACLOCAL_FLAGS
 #
+
+DIE=0
+
+(autoconf --version) < /dev/null >/dev/null 2>&1 || {
+   echo
+   echo "Error: You must have \`autoconf' installed to run $0."
+   echo "Get the latest version from ftp://ftp.gnu.org/pub/gnu/autoconf/"
+   NO_AUTOCONF=yes
+   DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+   echo 
+   echo "Error: You must have \`automake' installed to run $0."
+   echo "Get the latest version from ftp://ftp.gnu.org/pub/gnu/automake/"
+   NO_AUTOMAKE=yes
+   DIE=1
+}
+
+test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
+   echo
+   echo "Error: \`aclocal' appears to be missing. The installed version of"
+   echo "\`automake' may be too old. Get the most recent version from"
+   echo "ftp://ftp.gnu.org/pub/gnu/automake/"
+   NO_ACLOCAL=yes
+   DIE=1
+}
+
+if test "$DIE" -eq 1; then
+   exit 1
+fi
+
 echo "running aclocal $ACLOCAL_FLAGS ... "
 aclocal $ACLOCAL_FLAGS
 echo "running autoheader ... "
