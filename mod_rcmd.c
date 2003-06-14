2eff2ec4a0c0c29fdba1f0d4674a35d34e798006
Al Chu <chu11@llnl.gov>
2003-06-14 00:19:26 +0000

    renamed k4cmd -> krb4 in modules list krb4 module now compiled

--- mod_rcmd.c
+++ mod_rcmd.c
@@ -41,7 +41,7 @@
  * Ordered preference for determining default rcmd method
  *   Warning: If none of these are loaded, there will be no default.
  */
-static char * rcmd_rank[] = { "bsd", "mrsh", "ssh", "k4cmd", "qsh", "mqsh", NULL };
+static char * rcmd_rank[] = { "bsd", "mrsh", "ssh", "krb4", "qsh", "mqsh", NULL };
 
 typedef int (*RcmdInitF) (opt_t *);
 typedef int (*RcmdSigF)  (int efd, int signum);
