4e0f451f1734a882b32644aacaeee8bc512302e4
Mark A. Grondona <mgrondona@llnl.gov>
2004-02-12 22:50:24 +0000

    o added -X option to exclude targets based on genders attrs  o -i option now may be used to translate canonical <-> altnames    (if applicable)

--- src/modules/genders.c
+++ src/modules/genders.c
@@ -46,19 +46,30 @@
 
 #if STATIC_MODULES
 #  define pdsh_module_info genders_module_info
+#  define pdsh_module_priority genders_module_priority
 #endif    
 
-static hostlist_t genders_wcoll(opt_t *pdsh_opts);
-static void       genders_fini(void);
+int pdsh_module_priority = DEFAULT_MODULE_PRIORITY;
 
-static int genders_process_opt(opt_t *, int, char *);
-static hostlist_t _read_genders(char *attr, int iopt);
+
+/*
+ *  Static genders module interface routines:
+ */
+static hostlist_t genders_wcoll(opt_t *pdsh_opts);
+static int        genders_process_opt(opt_t *, int, char *);
+static void       genders_fini(void);
+static int        genders_postop(opt_t *);
+
 
 #if !GENDERS_G_ONLY
+
 static bool allnodes   = false;
-static bool altnames   = false;
+static bool opt_i      = false;
 #endif /* !GENDERS_G_ONLY */
+
+static genders_t gh    = NULL;
 static char *gend_attr = NULL;
+static char *excl_attr = NULL;
 
 /* 
  * Export pdsh module operations structure
@@ -67,7 +78,7 @@ struct pdsh_module_operations genders_module_ops = {
     (ModInitF)       NULL, 
     (ModExitF)       genders_fini, 
     (ModReadWcollF)  genders_wcoll, 
-    (ModPostOpF)     NULL,
+    (ModPostOpF)     genders_postop,
 };
 
 /* 
@@ -87,8 +98,11 @@ struct pdsh_module_option genders_module_options[] =
    { 'g', "attribute", "target nodes with specified genders attribute",
      DSH | PCP, (optFunc) genders_process_opt 
    },
+   { 'X', "attribute", "exclude nodes with specified genders attribute",
+     DSH | PCP, (optFunc) genders_process_opt
+   },
 #if !GENDERS_G_ONLY
-   { 'i', NULL,        "request canonical hostnames if applicable",
+   { 'i', NULL,        "request alternate or canonical hostnames if applicable",
      DSH | PCP, (optFunc) genders_process_opt
    },
    { 'a', NULL,        "target all nodes", 
@@ -117,15 +131,20 @@ struct pdsh_module pdsh_module_info = {
     &genders_module_options[0],
 };
 
-#if 0
-static int mod_genders_init(void)
-{
-    err("genders module initializing\n");
-    return 0;
-}
-#endif
+/*
+ * Static prototypes
+ */
+static genders_t  _handle_create();
+static hostlist_t _genders_to_altnames(genders_t g, hostlist_t hl);
+static hostlist_t _read_genders(char *attr);
+static void       _genders_opt_verify(opt_t *opt);
+static int        _delete_all (hostlist_t hl, hostlist_t dl);
 
-static int
+
+/*
+ *  Functions:
+ */
+
 genders_process_opt(opt_t *pdsh_opts, int opt, char *arg)
 {
     switch (opt) {
@@ -134,11 +153,14 @@ genders_process_opt(opt_t *pdsh_opts, int opt, char *arg)
         allnodes = true;
         break;
     case 'i':
-        altnames = true;
+        opt_i    = true;
         break;
 #endif /* !GENDERS_G_ONLY */
     case 'g':
         gend_attr = Strdup(arg);
+        break;
+    case 'X':
+        excl_attr = Strdup(arg);
         break;
     default:
         err("%p: genders_process_opt: invalid option `%c'\n", opt);
@@ -153,8 +175,74 @@ genders_fini(void)
 {
     if (gend_attr)
         Free((void **)&gend_attr);
+
+    if (excl_attr)
+        Free((void **)&excl_attr);
+
+    if ((gh != NULL) && (genders_handle_destroy(gh) < 0))
+        errx("%p: Error destroying genders handle: %s\n", genders_errormsg(gh));
+
     return;
 }
+
+static hostlist_t 
+genders_wcoll(opt_t *opt)
+{
+    _genders_opt_verify(opt);
+
+#if GENDERS_G_ONLY
+    if (!gend_attr)
+        return NULL;
+#else
+    if (!allnodes && !gend_attr)
+        return NULL;
+
+    if (allnodes)  
+        gend_attr = ALL_NODES;
+
+#endif /* !GENDERS_G_ONLY */
+
+    if (gh == NULL)
+        gh = _handle_create();
+
+    return _read_genders(gend_attr);
+}
+
+static int
+genders_postop(opt_t *opt)
+{
+    hostlist_t hl = NULL;
+
+    if (!opt->wcoll)
+        return (0);
+
+    if (gh == NULL)
+        gh = _handle_create();
+
+    if (excl_attr && (hl = _read_genders (excl_attr))) {
+        hostlist_t altlist = _genders_to_altnames (gh, hl);
+        _delete_all (opt->wcoll, hl);
+        _delete_all (opt->wcoll, altlist);
+
+        hostlist_destroy (altlist);
+        hostlist_destroy (hl);
+    }
+
+#if !GENDERS_G_ONLY
+    /*
+     *  Grab altnames if gend_attr or allnodes given and !opt_i,
+     *   or if opt_i and neither gend_attr or allnodes given.
+     */
+    if (  ( opt_i && !(gend_attr || allnodes))
+       || (!opt_i &&  (gend_attr || allnodes)) ) {
+        hostlist_t hl = opt->wcoll;
+        opt->wcoll = _genders_to_altnames(gh, hl);
+        hostlist_destroy(hl);
+    }
+#endif
+    return (0);
+}
+
 
 /* 
  * Verify options passed to this module
@@ -163,11 +251,11 @@ static void
 _genders_opt_verify(opt_t *opt)
 {
 #if !GENDERS_G_ONLY
-    if (altnames && !allnodes && (gend_attr == NULL)) {
-        err("%p: Warning: Ignoring -i without -a or -g\n");
-        altnames = false;
-    }
-
+/*    if (altnames && !allnodes && (gend_attr == NULL)) {
+ *       err("%p: Warning: Ignoring -i without -a or -g\n");
+ *       altnames = false;
+ *   }
+ */
     if (allnodes && (gend_attr != NULL))
         errx("%p: Do not specify -a with -g\n");
 #endif /* !GENDERS_G_ONLY */
@@ -180,69 +268,67 @@ _genders_opt_verify(opt_t *opt)
         if (gend_attr)
             errx("%p: Do not specify -g with other node selection options\n");
     }
+
+    return;
 }
 
-static hostlist_t 
-genders_wcoll(opt_t *opt)
+static int
+_maxnamelen (genders_t g)
 {
-    _genders_opt_verify(opt);
+    int maxvallen, maxnodelen;
+    if ((maxvallen = genders_getmaxvallen(g)) < 0)
+        errx("%p: genders: getmaxvallen: %s\n", genders_errormsg(g));
+    if ((maxnodelen = genders_getmaxvallen(g)) < 0)
+        errx("%p: genders: getmaxnodelen: %s\n", genders_errormsg(g));
 
-#if GENDERS_G_ONLY
-    if (!gend_attr)
-        return NULL;
-
-    /* GENDERS_G_ONLY, altnamesnot anissue, always
-     *use genders nodenames.
-     */
-    return _read_genders(gend_attr, true);
-#else
-    if (!allnodes && !altnames && !gend_attr)
-        return NULL;
-
-    if (allnodes)  
-        gend_attr = ALL_NODES;
-
-    return _read_genders(gend_attr, altnames);
-#endif /* !GENDERS_G_ONLY */
+    return (maxvallen > maxnodelen ? maxvallen : maxnodelen);
 }
 
 
 static hostlist_t
-_genders_to_altnames(genders_t g, char **nodes, int nnodes)
+_genders_to_altnames(genders_t g, hostlist_t hl)
 {
-    hostlist_t hl = NULL;
-    int maxvallen = 0;
+    hostlist_t retlist = NULL;
+    hostlist_iterator_t i = NULL;
+    int  maxlen = 0;
     char *altname = NULL;
     char *altattr = GENDERS_ALTNAME_ATTRIBUTE;
     char *host    = NULL;
-    int i, rc;
+    int  rc;
 
-    if ((hl = hostlist_create(NULL)) == NULL)
+    if ((retlist = hostlist_create(NULL)) == NULL)
         errx("%p: genders: hostlist_create: %m\n");
 
-    if ((maxvallen = genders_getmaxvallen(g)) < 0)
-        errx("%p: genders: getmaxvallen: %s\n", genders_errormsg(g));
+    maxlen = _maxnamelen (g);
+    altname = Malloc (maxlen + 1);
 
-    altname = Malloc(maxvallen + 1);
+    if ((i = hostlist_iterator_create(hl)) == NULL)
+        errx("%p: genders: hostlist_iterator_create: %m");
 
-    for (i = 0; i < nnodes; i++) {
-        memset(altname, '\0', maxvallen + 1);
+    while ((host = hostlist_next (i))) {
+        memset(altname, '\0', maxlen);
 
-        rc = genders_testattr(g, nodes[i], altattr, altname, maxvallen + 1);
-        if (rc < 0) 
-            errx("%p: genders: testattr failed: %s\n", genders_errormsg(g));
+        rc = genders_testattr(g, host, altattr, altname, maxlen + 1);
 
-        host = rc == 0 ? nodes[i] : altname;
-        if (hostlist_push_host(hl, host) <= 0)
+        /*
+         *  If node not found, attempt to lookup canonical name via
+         *   altername name.
+         */
+        if ((rc < 0) && (genders_errnum(g) == GENDERS_ERR_NOTFOUND)) 
+            rc = genders_getnodes (g, &altname, 1, altattr, host);
+
+        if (hostlist_push_host(retlist, (rc > 0 ? altname : host)) <= 0)
             err("%p: genders: warning: target `%s' not parsed: %m", host);
 
+        free(host);
     }
+
+    hostlist_iterator_destroy(i);
 
     Free((void **) &altname);
 
-    return hl;
+    return (retlist);
 }
-
 
 static hostlist_t 
 _genders_to_hostlist(genders_t gh, char **nodes, int nnodes)
@@ -259,11 +345,9 @@ _genders_to_hostlist(genders_t gh, char **nodes, int nnodes)
     }
 
     return hl;
-
 }
 
-static genders_t 
-_handle_create()
+static genders_t _handle_create()
 {
     genders_t gh = NULL;
 
@@ -302,42 +386,47 @@ _get_val(char *attr)
 
 
 static hostlist_t 
-_read_genders(char *attr, int iopt)
+_read_genders(char *attr)
 {
     hostlist_t hl = NULL;
-    genders_t  gh = NULL;
     char *val;
     char **nodes;
     int len, nnodes;
 
     val = _get_val(attr);
 
-    gh = _handle_create();
-
     if ((len = genders_nodelist_create(gh, &nodes)) < 0)
-        errx("%p: genders: nodelist_create: %s", genders_errormsg(gh));
+        errx("%p: genders: nodelist_create: %s\n", genders_errormsg(gh));
 
     if ((nnodes = genders_getnodes(gh, nodes, len, attr, val)) < 0) {
         errx("%p: Error querying genders for attr \"%s\": %s\n", 
                 attr ?: "(all)", genders_errormsg(gh));
     }
 
-    if (!iopt)
-        hl = _genders_to_altnames(gh, nodes, nnodes);
-    else
-        hl = _genders_to_hostlist(gh, nodes, nnodes);
+    hl = _genders_to_hostlist(gh, nodes, nnodes);
 
     if (genders_nodelist_destroy(gh, nodes) < 0) {
         errx("%p: Error destroying genders node list: %s\n",
                 genders_errormsg(gh));
     }
 
-    if (genders_handle_destroy(gh) < 0) {
-        errx("%p: Error destroying genders handle: %s\n",
-                genders_errormsg(gh));
-    }
 
     return hl;
+}
+
+static int 
+_delete_all (hostlist_t hl, hostlist_t dl)
+{
+    int                 rc   = 0;
+    char *              host = NULL;
+    hostlist_iterator_t i    = hostlist_iterator_create (dl);
+
+    while ((host = hostlist_next (i))) {
+        rc += hostlist_delete_host (hl, host);
+        free (host);
+    }
+    hostlist_iterator_destroy (i);
+    return (rc);
 }
 
 /*
