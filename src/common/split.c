f5058562a089a70ebdc77ed762c11fa83d2942d9
Mark A. Grondona <mgrondona@llnl.gov>
2006-03-30 20:55:59 +0000

    fix missing includes in split.c

--- src/common/split.c
+++ src/common/split.c
@@ -23,8 +23,15 @@
  *  with Pdsh; if not, write to the Free Software Foundation, Inc.,
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 \*****************************************************************************/
+
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
 #include <string.h>
+#include <stdio.h>
 #include "xmalloc.h"
+#include "xstring.h"
 #include "split.h"
 
 /* 
