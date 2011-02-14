67183f89640d9c4a70ffedb90b9ce44f065e353f
Mark A. Grondona <mark.grondona@gmail.com>
2011-02-14 23:42:17 +0000

    testsuite: Add basic functionality test for pdcp and rpdcp
    Test the most basic functionality of pdcp and rpdcp using the
    new pcptest module.

--- tests/t0006-pdcp.sh
+++ tests/t0006-pdcp.sh
@@ -17,6 +17,9 @@ fi
 test_expect_success 'Creating pdcp link to pdsh binary' '
     ln -s $PDSH_BUILD_DIR/src/pdsh/pdsh pdcp
 '
+test_expect_success 'Creating rpdcp link to pdsh binary' '
+    ln -s $PDSH_BUILD_DIR/src/pdsh/pdsh rpdcp
+'
 
 export PATH=.:$PATH
 
@@ -26,6 +29,9 @@ export PATH=.:$PATH
 #
 test_expect_success 'pdcp runs' '
 	pdcp -w foo -q * /tmp | tail -1 | grep foo
+'
+test_expect_success 'rpdcp runs' '
+	rpdcp -w foo -q * /tmp | tail -1 | grep foo
 '
 test_expect_success 'pdcp -V works' '
     pdcp -V 2>&1 | grep -q ^pdsh
@@ -49,7 +55,7 @@ test_expect_success '-e sets remote program path' '
 test_expect_success 'PDSH_REMOTE_PDCP_PATH sets remote program path' '
     tag="Remote program path"
 	path="/xxx/pdcp"
-	PDSH_REMOTE_PDCP_PATH=$path pdcp -w foo -q * /tmp | grep -q "$tag[ 	]*$path$" 
+	PDSH_REMOTE_PDCP_PATH=$path pdcp -w foo -q * /tmp | grep -q "$tag[ 	]*$path$"
 '
 test_expect_success '-f sets fanout' '
 	check_pdcp_option f Fanout 8
@@ -66,4 +72,58 @@ test_expect_success '-u sets command timeout' '
 test_expect_success 'command timeout 0 by default' '
     pdcp -w foo -q * /tmp | grep -q "Command timeout (secs)[ 	]*0$"
 '
+
+export T="$TEST_DIRECTORY/test-modules"
+
+test_expect_success 'Have pcptest rcmd module' '
+	PDSH_MODULE_DIR=$T pdcp -L | grep -q pcptest
+'
+
+setup_host_dirs() {
+	pdsh -w "$1" -Rexec mkdir %h
+}
+
+create_random_file() {
+	name=${1-testfile}
+	size=${2-1}
+	dd if=/dev/urandom of=$name bs=1024 count=$size >/dev/null 2>&1
+}
+
+test_expect_success 'pdcp basic functionality' '
+    HOSTS="host[0-10]"
+	setup_host_dirs "$HOSTS"
+	create_random_file testfile 10
+
+	PDSH_MODULE_DIR=$T pdcp -Rpcptest -w "$HOSTS" testfile testfile
+	if test $? -eq 0; then
+	  pdsh -SRexec -w "$HOSTS" diff -q testfile %h/testfile
+	  if test $? -eq 0; then
+	     return 0
+	  else
+	     say_color error "found a difference in pdcp output file"
+      fi
+   fi
+   false
+'
+rm -rf host* testfile
+
+test_expect_success 'rpdcp basic functionality' '
+	HOSTS="host[0-10]"
+	setup_host_dirs "$HOSTS"
+	pdsh -Rexec -w "$HOSTS" dd if=/dev/urandom of=%h/testfile bs=1024 count=10
+
+	mkdir output
+	PDSH_MODULE_DIR=$T rpdcp -Rpcptest -w "$HOSTS" testfile output/
+	if test $? -eq 0; then
+		pdsh -SRexec -w "$HOSTS" diff -q output/testfile.%h %h/testfile
+		if test $? -eq 0; then
+			return 0
+		else
+			say_color error "found a difference in pdcp output file"
+		fi
+	fi
+	false
+'
+rm -rf host* testfile
+
 test_done
