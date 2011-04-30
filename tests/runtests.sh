cd5eb32b5cc012d60637870868c2846b1379f2eb
Mark A. Grondona <mark.grondona@gmail.com>
2011-04-30 03:54:45 +0000

    testsuite: set and use SHELL_PATH in runtests.sh
    Set SHELL_PATH=/bin/sh in runtests.sh and override to bash
    for AIX (other systems may need this too). Run tests and
    aggregate-results.sh with SHELL_PATH.
    
    Also override GIT_TEST_CMP=cmp for AIX.
    
    Update Issue 28.
    
    runtests.sh now works to run tests on AIX

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 rm -rf test-results trash-directory*
 
+SHELL_PATH="/bin/sh"
+
+uname_s=$(uname -s)
+case "${uname_s}" in
+	AIX)  #  AIX /bin/sh is not functional with the testsuite
+	      SHELL_PATH=$(which bash)
+		  if [ -z "$SHELL_PATH" ]; then
+			  echo "This is AIX and I can't find bash. HELP!"
+		  fi
+		  GIT_TEST_CMP="cmp"
+		  export GIT_TEST_CMP SHELL_PATH
+		  ;;
+esac
+
 for test in t[0-9]*.sh; do
 	echo "*** $test ***"
-	./$test
+	$SHELL_PATH ./$test
 done
 
-./aggregate-results.sh test-results/*
+$SHELL_PATH aggregate-results.sh test-results/*
