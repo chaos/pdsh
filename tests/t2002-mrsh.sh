c207a6d51fdc220bcac7d25593896cb88f6ddfb4
Mark A. Grondona <mgrondona@llnl.gov>
2013-02-11 15:09:31 -0800

    testsuite: Add tests for mrsh
    Including most importantly a test for Issue 54.

---
+++ tests/t2002-mrsh.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+test_description='pdsh mrsh module tests'
+
+. ${srcdir:-.}/test-lib.sh
+
+if ! test_have_prereq MOD_RCMD_MRSH; then
+	skip_all='skipping mrsh tests, mrsh module not available'
+	test_done
+fi
+
+test_expect_success 'mrsh module runs' '
+	OUTPUT=$(pdsh -Rmrsh -w localhost echo i am here)
+'
+test_debug '
+	echo Output: "$OUTPUT"
+'
+test_expect_success 'mrsh localhost works' '
+	echo "$OUTPUT" | grep "localhost: i am here"
+'
+test_expect_success  'mrsh: -S generates empty lines (Issue 54)' '
+	OUTPUT=$(pdsh -Rmrsh -w localhost -S cd ..)
+	[ -z "$OUTPUT" ]
+'
+test_debug '
+	echo Output: "$OUTPUT"
+'
+
+test_done
