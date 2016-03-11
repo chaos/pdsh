87d86e7dab024da8b82fa1fd9d21e4e8b1995355
Mark A. Grondona <mgrondona@llnl.gov>
2016-03-10 20:21:57 -0800

    testsuite: avoid base64 -w option
    Avoid the -w option to base64 to support busybox base64 and other
    versions that do not have this option. Replace instead with use
    of xargs -n to join lines.

--- tests/t6036-long-output-lines.sh
+++ tests/t6036-long-output-lines.sh
@@ -7,12 +7,12 @@ Test that pdsh does not truncate very long lines'
 . ${srcdir:-.}/test-lib.sh
 
 test_expect_success 'pdsh does not truncate very long lines' '
-	dd if=/dev/urandom bs=1024 count=100 | base64 -w8000 > testfile &&
+	dd if=/dev/urandom bs=1024 count=100 | base64 | xargs -n10 > testfile &&
 	pdsh -w foo -N -Rexec cat testfile > output &&
 	test_cmp testfile output
 '
 test_expect_success 'pdsh does not truncate even longer lines' '
-	dd if=/dev/urandom bs=1024 count=100 | base64 -w80000 > testfile &&
+	dd if=/dev/urandom bs=1024 count=100 | base64 | xargs -n100 > testfile &&
 	pdsh -w foo -N -Rexec cat testfile > output &&
 	test_cmp testfile output
 '
