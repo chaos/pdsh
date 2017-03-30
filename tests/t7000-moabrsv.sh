c0071407d3648be46c4b305c4e2b3e774489e2fb
tabaer <troy@osc.edu>
2017-03-30 11:38:21 -0400

    Have moabrsv tests actually, you know, test stuff.

--- tests/t7000-moabrsv.sh
+++ tests/t7000-moabrsv.sh
@@ -19,3 +19,41 @@ if ! showres >/dev/null 2>&1; then
 fi
 
 
+#
+#  Ensure moabrsv module is loaded
+#
+export PDSH_MISC_MODULES=moabrsv
+
+RSVIDS=$(showres | egrep 'Job|User' | awk '{print $1}')
+if [ -n "$RSVID" ]; then
+   #
+   # Use an existing reservation
+   #
+   RSVID=$(echo $RSVIDS | tr ' ' '\n' | head -1)
+else
+   #
+   # Fail -- can't assume developer has permission to create a reservation
+   #
+   skip_all='skipping moab tests, unable to find a reservation'
+   test_done
+fi
+
+#
+#  Capture the nodes in reservation RSVID
+#
+NODES=$(mrsvctl -q --xml $RSVID | tr ' ' '\n' | grep AllocNodeList | sed 's/^AllocNodeList="//' | sed 's/"$//')
+
+test_expect_success 'smoabrsv -r option works' '
+	O=$(pdsh -r$RSVID -q | tail -1)
+	if test "x$O" != "x$NODES"; then
+	   say_color error "Error: pdsh -r$RSVID selected nodes $O expected $NOD
+ES"
+	   showres -n $RSVID 
+	   false
+        fi
+'
+
+test_expect_success 'moabrsv -r option handles illegal jobid gracefully' '
+	pdsh -r garbage 2>&1 | grep -q "invalid setting"
+'
+
