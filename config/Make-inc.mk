91a956e313877ff6bdb5244ed29b2018b9fa452f
Mark A. Grondona <mark.grondona@gmail.com>
2017-06-19 15:38:04 -0700

    rcmd: remove qcmd/mqcmd remote execution module
    Remove support for launching jobs on older Quadrics/Elan HW via
    the old (m)qcmd rcmd modules. There are very likely no more users
    of this interface.

--- config/Make-inc.mk
+++ config/Make-inc.mk
@@ -9,7 +9,6 @@
 #
 
 $(top_builddir)/src/common/libcommon.la \
-$(top_builddit)/src/qsnet/libqsw.la \
 $(top_builddit)/src/modules/libmods.la \
 : force-dependency-check
 	@cd `dirname $@` && make `basename $@`
