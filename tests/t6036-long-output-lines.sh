dccca8a760e773eca2534fc15491dda6dab3db7c
Mark A. Grondona <mark.grondona@gmail.com>
2017-06-24 12:26:40 -0700

    tests: t6036-long-output-lines: portability changes
    Check for missing base64 program and fall back to openssl if not
    found. Since `openssl base64` outputs in PEM format with newlines,
    use tr(1) to remove newlines and then wrap at desired long line
    length.

--- tests/t6036-long-output-lines.sh
+++ tests/t6036-long-output-lines.sh
@@ -6,15 +6,26 @@ Test that pdsh does not truncate very long lines'
 
 . ${srcdir:-.}/test-lib.sh
 
-test_expect_success 'pdsh does not truncate very long lines' '
-	dd if=/dev/urandom bs=1024 count=100 | base64 -w8000 > testfile &&
+if which base64 >/dev/null; then
+    base64="base64"
+elif which openssl >/dev/null; then
+    base64="openssl base64"
+else
+    skip_all 'failed to find base64 program'
+fi
+
+
+test_expect_success 'pdsh does not truncate very long lines' "
+	dd if=/dev/urandom bs=1024 count=100 | $base64 | tr -d '\n' | fold -w8000 > testfile &&
+	echo >>testfile &&
 	pdsh -w foo -N -Rexec cat testfile > output &&
 	test_cmp testfile output
-'
-test_expect_success 'pdsh does not truncate even longer lines' '
-	dd if=/dev/urandom bs=1024 count=100 | base64 -w80000 > testfile &&
-	pdsh -w foo -N -Rexec cat testfile > output &&
-	test_cmp testfile output
-'
+"
+test_expect_success 'pdsh does not truncate even longer lines' "
+	dd if=/dev/urandom bs=1024 count=100 | $base64 | tr -d '\n' | fold -w80000 > testfile2 &&
+        echo >>testfile2 &&
+	pdsh -w foo -N -Rexec cat testfile2 > output2 &&
+	test_cmp testfile2 output2
+"
 
 test_done
