29a6d52ef44a850e66f78ddc03c7d06b398a30d5
Mark A. Grondona <mark.grondona@gmail.com>
2019-08-15 21:08:16 -0700

    testsuite: test long output with no newlines
    Add t6114-no-newline-corruption.sh to test the fix for chaos/pdsh#114.
    Ensure that 1K - 10K output without newlines matches input.

---
+++ tests/t6114-no-newline-corruption.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+test_description='chaos/pdsh#114: pdsh garbles long output with no newline
+
+Test that pdsh handles lots of data with no newline.'
+
+. ${srcdir:-.}/test-lib.sh
+
+
+perl -e 'print "a" x 1024' > 1K
+perl -e 'print "a" x 8192' > 8K
+perl -e 'print "a" x 8195' > 8K+
+perl -e 'print "a" x 10000' > 10K
+
+for i in 1K 8K 8K+ 10K; do
+test_expect_success "pdsh does not garble $i with no newline" "
+	pdsh -w foo -N -Rexec cat $i > output.${i} &&
+	test_cmp ${i} output.${i}
+"
+done
+test_done
